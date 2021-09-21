3a4f5783c1376ec6b2b556b2a291409e38728b68
大灰 <tianze@192.168.124.101>
2021-09-21 16:18:05 +0800

    handle requests.

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,4 +1,7 @@
 use clap::{AppSettings, Clap};
+use std::{collections::HashMap, str::FromStr};
+use anyhow::{anyhow, Result};
+use reqwest::{header, Client, Response, Url};
 
 #[derive(Clap, Debug)]
 #[clap(version="1.0", author="Eggy")]
@@ -16,16 +19,73 @@ enum SubCommand {
 
 #[derive(Clap, Debug)]
 struct Get {
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+    Ok(s.into())
+}
+
+#[derive(Debug)]
+struct KvPair {
+    k: String,
+    v: String,
 }
 
 #[derive(Clap, Debug)]
 struct Post {
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
-    body: Vec<String>,
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: Vec<KvPair>,
 }
 
-fn main() {
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+async fn async_get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    println!("{:?}", resp.text().await?);
+    Ok(())
+}
+
+async fn async_post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    println!("{:?}", resp.text().await?);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let opts: Options = Options::parse();
     println!("{:?}", opts);
+
+    let client = Client::new();
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => async_get(client, args).await?,
+        SubCommand::Post(ref args) => async_post(client, args).await?,
+    };
+
+    Ok(result)
+
 }
